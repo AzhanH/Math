c56c874f8d7badf8d74646eec4e95afb4a6ad80e
Aun Raza <aun.raza@salsoft.net>
2022-12-14 11:35:48 +0500

    drawer navigation

---
+++ src/navigation/DrawerNavigator/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {createDrawerNavigator} from '@react-navigation/drawer'
+import DrawerContent from '../../components/DrawerComponents/DrawerScreenWrapper'
+import DrawerContent from '../../components/DrawerComponents/DrawerContent';
+
+
+
+
+
+const DrawerNavigator = _ =>{
+    return(
+        <DrawerNavigator>
+            
+        </DrawerNavigator>
+    )
+}
+
+export default DrawerNavigator;
